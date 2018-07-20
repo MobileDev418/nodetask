d6362f11386b300cab866b4b406c8d10305a59fa
MobileDev418 <oncemoreforyou@outlook.com>
2018-07-21 03:28:02 +0800

    fixed coding style issues

--- RPN/index.js
+++ RPN/index.js
@@ -1,13 +1,17 @@
 // Task 1
 /**
- * Devise a function that, given a string containing an expression in Reverse Polish notation, evaluates that expression, returning the result.
+ * Devise a function that, given a string containing
+ * an expression in Reverse Polish notation,
+ * evaluates that expression, returning the result.
  */
 
 // const notation = function(postfix) {
 //   var result = [];
 //   postfix = postfix.split(" ");
 //   for(var i = 0; i < postfix.length; i++) {
-//       if(postfix[i] != "+" && postfix[i] != "*" && postfix[i] != "-" && postfix[i] != "/" && postfix[i] != "=>") {
+//       if(postfix[i] != "+" && postfix[i] != "*"
+//          && postfix[i] != "-" && postfix[i] != "/"
+//          && postfix[i] != "=>") {
 //           result.push(postfix[i]);
 //       } else {
 //           var a = result.pop();
@@ -43,47 +47,42 @@
 
 // console.log(rpn('5 1 2 + 4 * + 3 -')) // prints "14"
 
-/*************************************** */
-const notation = function(postfix) {
-  var result = [];
-  postfix = postfix.split(" ");
+const notation = function (postfix) {
+  const result = [];
+  const postfix1 = postfix.split(' ');
 
-    postfix.forEach(element => {
-        if(element != "+" && element != "*" && element != "-" && element != "/" && element != "=>") {
-            result.push(element);
-        } else {
-            var a = result.pop();
-            var b = result.pop();
-            if(element === "+") {
-                result.push(parseInt(a) + parseInt(b));
-            } else if(element === "-") {
-                result.push(parseInt(b) - parseInt(a));
-            } else if(element === "*") {
-                result.push(parseInt(a) * parseInt(b));
-            } else if(element === "/") {
-                result.push(parseInt(b) / parseInt(a));
-            } else if(element === "^") {
-                result.push(Math.pow(parseInt(b), parseInt(a)));
-            }
-        }
-    });
-
-    if(result.length > 1) {
-        return "error";
+  postfix1.forEach((element) => {
+    if (element !== '+' && element !== '*' && element !== '-' && element !== '/' && element !== '=>') {
+      result.push(element);
     } else {
-        return result.pop();
+      const a = result.pop();
+      const b = result.pop();
+      if (element === '+') {
+        result.push(parseInt(a, 10) + parseInt(b, 10));
+      } else if (element === '-') {
+        result.push(parseInt(b, 10) - parseInt(a, 10));
+      } else if (element === '*') {
+        result.push(parseInt(a, 10) * parseInt(b, 10));
+      } else if (element === '/') {
+        result.push(parseInt(b, 10) / parseInt(a, 10));
+      }
     }
-}
+  });
 
-console.log(notation("5 2 + 3 *"));
-console.log(notation("5 2 * 2 * 4 +"));
-console.log(notation("5"));
-console.log(notation("0 1 *"));
+  if (result.length > 1) {
+    return 'error';
+  }
+  return result.pop();
+};
+
+console.log(notation('5 2 + 3 *'));
+console.log(notation('5 2 * 2 * 4 +'));
+console.log(notation('5'));
+console.log(notation('0 1 *'));
 
 // let arr = ["Hello", "World", "Testing"];
 
 // arr.map( one => one.toUpperCase())
 //     .filter(one => one.length <= 5)
 //     .reduce((acc, curr) => acc + curr + ' ', '');
-    
 // console.log();
